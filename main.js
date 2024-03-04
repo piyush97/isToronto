bb3e9eb4b7bb4172cd5f117e4355d19d32553eac
Piyush Mehta <me@piyushmehta.com>
2024-03-04 23:50:41 +0000

    JSDocs

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
+// Import the necessary modules
 const fs = require("fs").promises;
 const path = require("path");
 
-// Define the boundaries of Toronto
+// Define the boundaries of Toronto using environment variables or default values
 const latMin = process.env.LAT_MIN || 43.403221;
 const latMax = process.env.LAT_MAX || 43.855401;
 const longMin = process.env.LONG_MIN || -79.639319;
 const longMax = process.env.LONG_MAX || -79.115819;
 
-// File paths
+// Define the input and output file paths using environment variables or default values
 const inputFilePath = path.resolve(process.env.INPUT_FILE || "data.json");
 const outputFilePath = path.resolve(
   process.env.OUTPUT_FILE || "filteredData.json"
 );
 
-// Use a Set to store unique lat-long pairs
+// Initialize a Set to store unique lat-long pairs
 const uniqueLocations = new Set();
 
+/**
+ * Main function to filter the locations in the input file
+ *
+ */
 async function filterLocations() {
   try {
-    // Read the data from the file
+    // Read the data from the input file
     const data = await fs.readFile(inputFilePath, "utf8");
 
     // Parse the JSON data
@@ -28,6 +33,7 @@ async function filterLocations() {
     const filteredData = jsonData
       .filter(
         (item) =>
+          // Check if the location is within the defined boundaries
           item.latitude >= latMin &&
           item.latitude <= latMax &&
           item.longitude >= longMin &&
@@ -46,18 +52,24 @@ async function filterLocations() {
         uniqueLocations.add(key);
         return true;
       })
-      .map((item) => ({ latitude: item.latitude, longitude: item.longitude })); // map to new object with only lat and long
+      .map((item) =>
+        // Return a new object with only the latitude and longitude
+        ({ latitude: item.latitude, longitude: item.longitude })
+      );
 
     // Stringify the filtered data
     const filteredDataString = JSON.stringify(filteredData, null, 2);
 
-    // Write the filtered data to a new JSON file
+    // Write the filtered data to the output file
     await fs.writeFile(outputFilePath, filteredDataString);
 
+    // Log a success message
     console.log(`Filtered data written to ${outputFilePath}`);
   } catch (error) {
+    // Log any errors that occur
     console.error(`An error occurred: ${error.message}`);
   }
 }
 
+// Call the main function
 filterLocations();
